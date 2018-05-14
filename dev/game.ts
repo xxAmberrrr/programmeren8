616ac2fa3804e404bb52f3def8bbe62498c6e01c
Amber <amber__12@hotmail.com>
2018-05-14 13:35:25 +0200

    Changed game class into singleton

--- dev/game.ts
+++ dev/game.ts
@@ -1,7 +1,32 @@
 class Game {
-    constructor(){
-        console.log("new game created!")
+    private static instance: Game
+
+    private coins:number = 0
+    private textfield:HTMLElement
+
+    private constructor(){
+        this.textfield = document.getElementsByTagName("textfield")[0] as HTMLElement
+
+        this.gameLoop()
+    }
+
+    public static getInstance() {
+        if(! Game.instance) {
+            Game.instance = new Game()
+        }
+        return Game.instance
+    }
+
+    private gameLoop():void {
+        requestAnimationFrame(() => this.gameLoop())
+    }
+
+    public score() {
+        this.coins ++
+        this.textfield.innerHTML = "Score: " + this.coins
     }
 }
 
-window.addEventListener("load", () => new Game())
+window.addEventListener("load", () => {
+    Game.getInstance()
+})
